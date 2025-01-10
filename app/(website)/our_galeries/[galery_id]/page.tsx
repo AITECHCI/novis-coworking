9af2c36eeee4a15298d525f23009c4de596fbb6e
AITECHCI <aitechci1@gmail.com>
2025-01-10 22:42:59 +0000

    Correction Galery

--- app/(website)/our_galeries/[galery_id]/page.tsx
+++ app/(website)/our_galeries/[galery_id]/page.tsx
@@ -2,9 +2,15 @@ import HeaderPage from "@/components/galeries/header-page";
 import DescriptionAllSpaces from "@/components/galeries/DescriptionAllSpaces";
 import { galeries } from "@/config/data";
 
-export default function GaleryPage({ params }: { params: { galery_id: string } }) {
-  const { galery_id } = params; // Pas besoin de 'await' ici
-  const galerie = galeries.find((galerie) => galerie.id === galery_id);
+interface GalleryPageProps {
+  params: {
+    galery_id: string;
+  };
+}
+
+export default async function GaleryPage({ params }: GalleryPageProps) {
+  const { galery_id } = await params; // Utilisation de await pour accéder à galery_id
+  const galerie = galeries.find((gal) => gal.id === galery_id);
 
   if (!galerie) {
     return <div>Galerie non trouvée</div>; // Gestion de l'erreur
